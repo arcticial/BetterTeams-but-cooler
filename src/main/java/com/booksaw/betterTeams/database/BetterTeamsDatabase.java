aed0518872f141054f8c910bb0fcfb66b3452659
sammi <maltrolling@gmail.com>
2026-06-30 16:43:11 -0400

    pls

--- src/main/java/com/booksaw/betterTeams/database/BetterTeamsDatabase.java
+++ src/main/java/com/booksaw/betterTeams/database/BetterTeamsDatabase.java
@@ -36,86 +36,30 @@ public class BetterTeamsDatabase extends Database {
 
 		createTableIfNotExists(TableName.ALLIES,
 				"team1ID VARCHAR(50) NOT NULL, team2ID VARCHAR(50) NOT NULL, PRIMARY KEY(team1ID, team2ID), FOREIGN KEY (team1ID) REFERENCES "
-						+ TableName.TEAM + "(teamID) ON DELETE CASCADE, FOREIGN KEY (team2ID) REFERENCES "
+						+ TableName.TEAM
+						+ "(teamID) ON DELETE CASCADE, FOREIGN KEY (team2ID) REFERENCES "
 						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
-		createTableIfNotExists(TableName.TEAM_META,
-				"teamID VARCHAR(50) NOT NULL, metaKey VARCHAR(255) NOT NULL, metaValue TEXT, PRIMARY KEY(teamID, metaKey), FOREIGN KEY (teamID) REFERENCES "
-						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
-
-		// Add anchor columns if they don't exist
-		if (!hasColumn(TableName.TEAM, "anchor")) {
-			addColumn(TableName.TEAM, "anchor", "BOOLEAN DEFAULT 0", "pvp", true);
-		}
-
-		if (!hasColumn(TableName.PLAYERS, "anchor")) {
-			addColumn(TableName.PLAYERS, "anchor", "BOOLEAN DEFAULT 0", "title", true);
-		}
 	}
 
 	public PreparedStatement select(String select, TableName from) {
-		return executeQuery("SELECT ? FROM ?", select, from.toString());
+		return executeQuery("SELECT %s FROM %s".formatted(select, from.toString()));
 	}
 
-	/**
-	 * @param select the element to select
-	 * @param from   the table which the data is from
-	 * @param where  the condition required for the data to be included
-	 * @return the resultant resultSet
-	 */
 	public PreparedStatement selectWhere(String select, TableName from, String where) {
-		return executeQuery("SELECT ? FROM ? WHERE ?;", select, from.toString(), where);
+		return executeQuery("SELECT %s FROM %s WHERE %s;".formatted(select, from.toString(), where));
 	}
 
-	/**
-	 * @param select  the element to select
-	 * @param from    the table which the data is from
-	 * @param where   the condition required for the data to be included
-	 * @param orderBy what to order the data by
-	 * @return the resultant resultset
-	 */
-	public PreparedStatement selectWhereOrder(String select, TableName from, String where, String orderBy) {
-		return executeQuery("SELECT ? FROM ? WHERE ? ORDER BY ?;", select, from.toString(), where, orderBy);
-	}
-
-	/**
-	 * @param select  the element to select
-	 * @param from    the table which the data is from
-	 * @param orderBy what to order the data by
-	 * @return The resultant resultset
-	 */
 	public PreparedStatement selectOrder(String select, TableName from, String orderBy) {
-		return executeQuery("SELECT ? FROM ? ORDER BY ?;", select, from.toString(), orderBy);
-	}
-
-	/**
-	 * @param select      the element to select
-	 * @param table       the table which the data is from
-	 * @param joinTable   the table to join
-	 * @param columToJoin the details of the join
-	 * @param orderBy     the order by conditions
-	 * @return The resultSet of the select
-	 */
-	public PreparedStatement selectInnerJoinOrder(String select, TableName table, TableName joinTable, String columToJoin,
-												  String orderBy) {
-		return executeQuery("SELECT ? FROM ? INNER JOIN ? on (?) ORDER BY ?;", select, table.toString(),
-				joinTable.toString(), columToJoin, orderBy);
+		return executeQuery("SELECT %s FROM %s ORDER BY %s;".formatted(select, from.toString(), orderBy));
 	}
 
 	public PreparedStatement selectInnerJoinGroupByOrder(String select, TableName table, TableName joinTable,
 														 String columToJoin, String groupBy, String orderBy) {
-		return executeQuery("SELECT ? FROM ? INNER JOIN ? on (?) GROUP BY ? ORDER BY ?;", select, table.toString(),
-				joinTable.toString(), columToJoin, groupBy, orderBy);
+		return executeQuery("SELECT %s FROM %s INNER JOIN %s on (%s) GROUP BY %s ORDER BY %s;".formatted(select,
+				table.toString(), joinTable.toString(), columToJoin, groupBy, orderBy));
 	}
 
-	/**
-	 * Used to check if an SQL query has a result
-	 *
-	 * @param from  the table which the data is from
-	 * @param where the condition required for the data to be included
-	 * @return if the query has a result
-	 */
 	public boolean hasResult(TableName from, String where) {
-
 		try (PreparedStatement ps = selectWhere("*", from, where)) {
 			ResultSet result = ps.executeQuery();
 			if (result == null) {
@@ -126,20 +70,9 @@ public class BetterTeamsDatabase extends Database {
 			e.printStackTrace();
 			return false;
 		}
-
 	}
 
-	/**
-	 * Used to a specific column value from the
-	 *
-	 * @param column The column name
-	 * @param from   the table
-	 * @param where  the condition
-	 * @return the first returned result, the specified column. Will return "" if an
-	 * error occurs
-	 */
 	public String getResult(String column, TableName from, String where) {
-
 		try (PreparedStatement pr = selectWhere(column, from, where)) {
 			ResultSet results = pr.executeQuery();
 			results.first();
@@ -147,57 +80,33 @@ public class BetterTeamsDatabase extends Database {
 		} catch (SQLException e) {
 			return "";
 		}
-
 	}
 
-	/**
-	 * Used to delete a record from the specified table
-	 *
-	 * @param table     The table to update
-	 * @param condition The condition for the update
-	 */
 	public void deleteRecord(TableName table, String condition) {
-		executeStatement("DELETE FROM ? WHERE ?;", table.toString(), condition);
-
+		executeStatement("DELETE FROM %s WHERE %s;".formatted(table.toString(), condition));
 	}
 
 	/**
-	 * Used to modify a record in the database
+	 * imagine 
 	 *
 	 * @param table     the table the record is in
-	 * @param update    the values to update (ie "col = exp1, col2 = exp2")
-	 * @param condition The condition for which records should be updated
+	 * @param field     the column to update
+	 * @param update    the value to set (bound safely via PreparedStatement)
+	 * @param condition the goon condition
 	 */
-	public void updateRecordWhere(TableName table, String update, String condition) {
-		executeStatement("UPDATE ? SET ? WHERE ?;", table.toString(), update, condition);
+	public void updateRecordWhere(TableName table, String field, Object update, String condition) {
+		executeStatement("UPDATE %s SET %s = ? WHERE %s;".formatted(table.toString(), field, condition), update);
 	}
 
-	/**
-	 * Used to modify all records in a database
-	 *
-	 * @param table  the table to modify
-	 * @param update the value to update (ie "col = exp1, col2 = exp2")
-	 */
 	public void updateRecord(TableName table, String update) {
-		executeStatement("UPDATE ? SET ?", table.toString(), update);
+		executeStatement("UPDATE %s SET %s".formatted(table.toString(), update));
 	}
 
-	/**
-	 * Used to add a record into a table
-	 *
-	 * @param table   The table to insert the record into
-	 * @param columns The columns that data is being provided for (ie "col1, col2,
-	 *                col3)
-	 * @param values  the values to insert (ie "val1, val2, val3")
-	 */
 	public void insertRecord(TableName table, String columns, String values) {
-		executeStatement("INSERT INTO ? (?) VALUES (?);", table.toString(), columns, values);
+		executeStatement("INSERT INTO %s (%s) VALUES (%s);".formatted(table.toString(), columns, values));
 	}
 
 	public void insertRecordIfNotExists(TableName table, String columns, String values) {
-//		executeStatement("IF NOT EXISTS (SELECT * FROM ? WHERE ?) INSERT INTO ? (?) VALUES(?);", table.toString(),
-//				condition, table.toString(), columns, values);
-		executeStatement("INSERT IGNORE INTO ? (?) VALUES (?);", table.toString(), columns, values);
+		executeStatement("INSERT IGNORE INTO %s (%s) VALUES (%s);".formatted(table.toString(), columns, values));
 	}
-
 }
